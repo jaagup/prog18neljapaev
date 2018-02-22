22c12fd6ed484ee9c7663a732ea210377732fecd
zeus <zeus@A406-0.INFORMAATIKA>
2018-02-22 09:13:22 +0200

    try näide

--- vorkandmed/src/main/java/ee/tlu/jaagup/vorkandmed/Vork1.java
+++ vorkandmed/src/main/java/ee/tlu/jaagup/vorkandmed/Vork1.java
@@ -2,7 +2,8 @@ package ee.tlu.jaagup.vorkandmed;
 import java.net.*;
 import java.io.*;
 public class Vork1 {
-    public static void main(String[] arg) throws Exception{
+    public static void main(String[] arg) {
+      try{
        String aadress="http://www.tlu.ee/~jaagup/andmed/muu/ounad/antoonovka2.txt";
        BufferedReader br=new BufferedReader(new InputStreamReader(
           new URL(aadress).openStream()
@@ -10,11 +11,25 @@ public class Vork1 {
        String rida=br.readLine();
        System.out.println("Tulbad: "+rida);
        rida=br.readLine();
+       double[] summad=new double[rida.split(",").length];
+       int kogus=0;
        while(rida!=null){
           String[] m=rida.split(",");
-          System.out.println(m[0]);
+          for(int i=0; i<m.length; i++){
+             summad[i]+=Double.parseDouble(m[i]);
+          }
+          kogus++;
+//          System.out.println(m[0]);
           rida=br.readLine();
        }
        br.close();
+       System.out.println("Augusti summa: "+summad[0]);
+       System.out.println("Septembri keskmine: "+summad[1]/kogus);
+      }catch(Exception ex){
+         System.out.println("Probleem: "+ex);
+      }
+       //Kuvage augusti diameetrite summa ning septembri diameetrite keskmine
+       //Looge uus programm, kuvage samade õunte augusti ja septembri
+       //diameetrid XY-joonisel
     }
 }
