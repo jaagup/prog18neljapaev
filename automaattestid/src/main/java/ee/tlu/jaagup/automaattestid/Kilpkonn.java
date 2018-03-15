afa189b7d4c5d0f1428896e5b5828479cc17aedd
zeus <zeus@A406-0.INFORMAATIKA>
2018-03-15 09:10:03 +0200

    Keeramine

--- automaattestid/src/main/java/ee/tlu/jaagup/automaattestid/Kilpkonn.java
+++ automaattestid/src/main/java/ee/tlu/jaagup/automaattestid/Kilpkonn.java
@@ -1,12 +1,25 @@
 package ee.tlu.jaagup.automaattestid;
 public class Kilpkonn {
     int x;
+    int y;
+    int suund=1;
     public Kilpkonn(int kx, int ky) {
         x=kx;
+        y=ky;
     }
-
     int kysiX() {
         return x;
+    }   
+
+    int kysiY() {
+        return y;
     }
-    
+
+    int kysiSuund() {
+        return suund;
+    }
+
+    void keeraParemale() {
+        suund++;
+    }
 }
--- automaattestid/src/test/java/ee/tlu/jaagup/automaattestid/Testid1.java
+++ automaattestid/src/test/java/ee/tlu/jaagup/automaattestid/Testid1.java
@@ -2,6 +2,7 @@ package ee.tlu.jaagup.automaattestid;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class Testid1 {
     
@@ -11,8 +12,21 @@ public class Testid1 {
     public void esimene(){
         assertEquals(5, 3+2); 
     }    
-    @Test public void teine(){
-       Kilpkonn k=new Kilpkonn(3, 5);
+    Kilpkonn k;
+    @Before
+    public void setUp(){
+       k=new Kilpkonn(3, 5);
+       //System.out.println("Loodi kilpkonn");
+    }    
+    @Test public void loomine(){
        assertEquals(3, k.kysiX());
+       assertEquals(5, k.kysiY());
+       assertEquals(1, k.kysiSuund()); 
+       //Lisage test ja kood ka y-koordinaadi tarbeks
+    }
+    @Test public void keeramine(){
+       assertEquals(1, k.kysiSuund()); 
+       k.keeraParemale(); 
+       assertEquals(2, k.kysiSuund()); 
     }
 }
